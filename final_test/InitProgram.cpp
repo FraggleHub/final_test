fe13abb8b8dc2e50aac76ac6515887fc12d1a994
Denis <fikinsd@gmail.com>
2024-05-02 17:12:35 +0300

    123

---
+++ final_test/InitProgram.cpp
@@ -0,0 +1,15 @@
+#include "InitProgram.h"
+#include <iostream>
+#include "Accounts.h"
+
+using namespace std;
+
+Accounts* acc = new Accounts[1];
+
+InitProgram::InitProgram() {
+	while (true) {
+		
+		cout << "Добро пожаловать!" << endl;
+		cout << "Пользователей не найдено, просьба добавить" << endl;
+	}
+}
---
+++ final_test/InitProgram.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class InitProgram {
+protected:
+	string text;
+	int number;
+	int choice;
+public:
+	InitProgram();
+};
